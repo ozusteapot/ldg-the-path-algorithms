e12bf0f7a99f7ea9e81b9f03642bf4c6f01949ec
Alex Samson <samsonioanalexandru@gmail.com>
2021-10-08 16:44:31 +0300

    first commit

--- src/LinkedList/pairs/lorena-alex/LinkedList.js
+++ src/LinkedList/pairs/lorena-alex/LinkedList.js
@@ -1,4 +1,26 @@
-module.exports = class LinkedList {
-    constructor() {
+class LinkedList {
+    constructor() { 
+        this.head = null;
+        this.tail = null;
+        this.nodeCount = 0;
+    }
+​
+    push(){
+​
+    }
+​
+    pop(){
+​
+    }
+} 
+​
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
     }
 }
+​
+module.exports = {
+    LinkedList: LinkedList, 
+    Node: Node};
--- tests/LinkedList/pairs/lorena-alex/LinkedList.test.js
+++ tests/LinkedList/pairs/lorena-alex/LinkedList.test.js
@@ -1,8 +1,21 @@
-const LinkedList = require(‘../../../../src/LinkedList/pairs/lorena-alex/LinkedList.js’)
-
-describe(‘LinkedList’, () => {
-    test(‘can create a new list’, () => {
-        const list = new LinkedList()
+const Solution = require('../../../../src/LinkedList/pairs/lorena-alex/LinkedList.js')
+// const LinkedList = new Solution.LinkedList();
+// const Node = new Solution.Node();
+​
+describe('Solution', () => {
+    const list = new Solution.LinkedList();
+    const node = new Solution.Node();
+    test('can create a new list', () => {  
         expect(list).toBeDefined();
     })
+​
+    test('check for an empty list', () => {
+        expect(list).toEqual(expect.any(Solution.LinkedList));
+    })
+​
+    test('check for existing node', () => {
+        expect(node).toBeDefined();
+    })
+​
+    
 })
